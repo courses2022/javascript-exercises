7cccb38754f326daade86a05be0f03554a18a4fb
rajpranz <rajeshwarrprasad@yahoo.com>
2022-05-14 23:37:30 +1200

    Complete test 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
+const repeatString = function(data, count) {
 
+    if(count < 0) return 'ERROR'
+    else
+        return data.repeat(count);
 };
 
 // Do not edit below this line
